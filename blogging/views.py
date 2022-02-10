488af011a4fdcbf0447dce662ae544bb200e5abc
Brent Kieszling <bkieszling@yahoo.com>
2022-02-10 12:07:18 -0800

    Lesson-08 Homework for review. Updated list view and detail view to classes.

--- blogging/views.py
+++ blogging/views.py
@@ -2,21 +2,20 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render
 from django.template import loader
 from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+class PubPostListView(ListView):
+    pub_posts = 'blog_index'
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+    template_name = 'blogging/list.html'
+    
+
+class PostDetailView(DetailView):
+    detail_posts = 'blog_detail'
+    queryset = Post.objects.exclude(published_date__exact=None)
+    template_name = 'blogging/detail.html'
 
 def stub_view(requests, *args, **kwargs):
     body = "Stub View\n\n"
@@ -28,4 +27,3 @@ def stub_view(requests, *args, **kwargs):
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
 
-# Create your views here.
