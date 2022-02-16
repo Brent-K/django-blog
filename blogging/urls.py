a98c3ae91b7a6ac0abc18c4b11013bc728245aaf
Brent Kieszling <bkieszling@yahoo.com>
2022-02-16 11:01:40 -0800

    Applied Black.

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,8 +1,7 @@
-
 from django.urls import path
 from blogging.views import stub_view, PubPostListView, PostDetailView
 
 urlpatterns = [
-        path('', PubPostListView.as_view(), name="blog_index"),
-        path('posts/<int:pk>/', PostDetailView.as_view(), name="blog_detail"),
-        ]
+    path("", PubPostListView.as_view(), name="blog_index"),
+    path("posts/<int:pk>/", PostDetailView.as_view(), name="blog_detail"),
+]
